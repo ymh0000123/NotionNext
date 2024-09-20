29ea46500e174d3891826b3741e0ceb9629fb551
ymh0000123 <ymh0000123@163.com>
2024-09-20 20:14:33 +0800

    Revert "ai-summary"
    This reverts commit 5ae52fa207e3311537b47ed0805369b6862d36f2.

--- blog.config.js
+++ blog.config.js
@@ -275,7 +275,7 @@ const BLOG = {
     'https://cdn1.tianli0.top/gh/zhheo/Post-Abstract-AI@0.15.2/tianli_gpt.css',
   TianliGPT_JS:
     process.env.NEXT_PUBLIC_TIANLI_GPT_JS ||
-    'https://resource-files.xiao-feishu.top/js/ai-summary-2.js',
+    'https://resource-files.xiao-feishu.top/js/ai-summary.js',
   TianliGPT_KEY: process.env.NEXT_PUBLIC_TIANLI_GPT_KEY || '6owaw57iyxndxv5rftc2g4ao6xo67tea',
 
   // Chatbase 是否显示chatbase机器人 https://www.chatbase.co/
